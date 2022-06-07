8d7f7201f6954119ced75372a507f1246e36e03a
nashaofu <diaocheng@outlook.com>
2022-06-07 22:29:29 +0800

    fix: 修复mac与windows bug

--- src/darwin.rs
+++ src/darwin.rs
@@ -5,9 +5,12 @@ pub fn capture_display(screen_capturer: &ScreenCapturer) -> Option<Image> {
   let cg_display = CGDisplay::new(screen_capturer.display_info.id);
   let cg_image = cg_display.image()?;
 
-  Some(Image::from_bgr(
+  match Image::from_bgr(
     cg_image.width() as u32,
     cg_image.height() as u32,
     Vec::from(cg_image.data().bytes()),
-  ))
+  ) {
+    Ok(image) => Some(image),
+    Err(_) => None,
+  }
 }
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,4 +1,11 @@
-// mod x11;
-// #[cfg(feature="wayland")]
 mod wayland;
-pub use wayland::*;
+mod x11;
+
+pub fn wayland_capture_display(screen_capturer: &ScreenCapturer) -> Option<Image> {
+  if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
+    println!("Think Different!");
+    wayland_capture_display()
+  } else {
+    x11_capture_display()
+  }
+}
