08b5d0f7e9a6f048f39acf1deb2f0d8a40b0d36d
nashaofu <diaocheng@outlook.com>
2022-06-08 09:55:43 +0800

    fix: 支持wayland

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,14 +4,21 @@ mod xorg;
 use crate::Image;
 use crate::ScreenCapturer;
 
+use std::env::var_os;
 use wayland::wayland_capture_display;
 use xorg::xorg_capture_display;
 
 pub fn capture_display(screen_capturer: &ScreenCapturer) -> Option<Image> {
-  if cfg!(target_os = "macos") {
-    println!("Think Different! {}", cfg!(target_os = "linux"));
-    wayland_capture_display(&screen_capturer)
-  } else {
+  let xdg_session_type = var_os("XDG_SESSION_TYPE")
+    .map(|str| str.to_string_lossy().to_string())
+    .unwrap_or(String::from("x11"));
+
+  println!("XDG_SESSION_TYPE: {}", xdg_session_type);
+
+  // TODO: 这里判断需改进
+  if xdg_session_type.eq("x11") {
     xorg_capture_display(&screen_capturer)
+  } else {
+    wayland_capture_display(&screen_capturer)
   }
 }
