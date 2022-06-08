320019c21994fb738e6ea053825ad6a6ed49f8e2
nashaofu <diaocheng@outlook.com>
2022-06-08 07:34:27 -0700

    feat: linux 优先用x11协议截图，失败了再使用wayland截图

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,21 +4,13 @@ mod xorg;
 use crate::Image;
 use crate::ScreenCapturer;
 
-use std::env::var_os;
+use std::panic;
 use wayland::wayland_capture_display;
 use xorg::xorg_capture_display;
 
 pub fn capture_display(screen_capturer: &ScreenCapturer) -> Option<Image> {
-  let xdg_session_type = var_os("XDG_SESSION_TYPE")
-    .map(|str| str.to_string_lossy().to_string())
-    .unwrap_or(String::from("x11"));
-
-  println!("XDG_SESSION_TYPE: {}", xdg_session_type);
-
-  // TODO: 这里判断需改进
-  if xdg_session_type.eq("x11") {
-    xorg_capture_display(&screen_capturer)
-  } else {
-    wayland_capture_display(&screen_capturer)
+  match panic::catch_unwind(|| xorg_capture_display(&screen_capturer)) {
+    Ok(image) => image,
+    Err(_) => wayland_capture_display(&screen_capturer),
   }
 }
