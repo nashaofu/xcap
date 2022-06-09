cd4604c28065a0414dd2e0f2a13701dfda54e48a
nashaofu <diaocheng@outlook.com>
2022-06-09 08:29:02 -0700

    feat: 检测wayland

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,13 +4,28 @@ mod xorg;
 use crate::Image;
 use crate::ScreenCapturer;
 
-use std::panic;
+use std::env::var_os;
 use wayland::wayland_capture_display;
 use xorg::xorg_capture_display;
 
+fn wayland_dectected() -> bool {
+  let xdg_session_type = var_os("XDG_SESSION_TYPE")
+    .unwrap_or_default()
+    .to_string_lossy()
+    .to_string();
+
+  let wayland_display = var_os("WAYLAND_DISPLAY")
+    .unwrap_or_default()
+    .to_string_lossy()
+    .to_string();
+
+  return xdg_session_type.eq("wayland") || wayland_display.to_lowercase().contains("wayland");
+}
+
 pub fn capture_display(screen_capturer: &ScreenCapturer) -> Option<Image> {
-  match panic::catch_unwind(|| xorg_capture_display(&screen_capturer)) {
-    Ok(image) => image,
-    Err(_) => wayland_capture_display(&screen_capturer),
+  if wayland_dectected() {
+    wayland_capture_display(&screen_capturer)
+  } else {
+    xorg_capture_display(&screen_capturer)
   }
 }
