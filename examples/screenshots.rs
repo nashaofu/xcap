e6434afc597c82a500a4533569108328f9efe058
nashaofu <diaocheng@outlook.com>
2022-07-11 22:03:09 +0800

    feat: mac 支持截取指定屏幕区域

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -9,15 +9,15 @@ fn main() {
     println!("capturer {:?}", screen);
     let image = screen.capture().unwrap();
     let buffer = image.buffer();
-    fs::write(format!("{}.png", screen.id.to_string()), &buffer).unwrap();
+    fs::write(format!("target/{}.png", screen.id.to_string()), &buffer).unwrap();
   }
 
   let screen = Screen::from_point(100, 100).unwrap();
   println!("capturer {:?}", screen);
 
-  let image = screen.capture().unwrap();
+  let image = screen.capture_area(300, 300, 1680, 1680).unwrap();
   let buffer = image.buffer();
-  fs::write("capture_display_with_point.png", &buffer).unwrap();
+  fs::write("target/capture_display_with_point.png", &buffer).unwrap();
 
   println!("运行耗时: {:?}", start.elapsed());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -57,4 +57,41 @@ impl Screen {
   pub fn capture(&self) -> Option<Image> {
     capture_screen(&self)
   }
+
+  pub fn capture_area(&self, x: i32, y: i32, width: u32, height: u32) -> Option<Image> {
+    let screen_x2 = self.x + self.width as i32;
+    let screen_y2 = self.y + self.height as i32;
+
+    let mut x1 = x;
+    let mut y1 = y;
+    let mut x2 = x1 + width as i32;
+    let mut y2 = x2 + height as i32;
+
+    // x y 必须在屏幕范围内
+    if x1 < self.x {
+      x1 = self.x;
+    } else if x1 > screen_x2 {
+      x1 = screen_x2
+    }
+
+    if y1 < self.y {
+      y1 = self.y;
+    } else if y1 > screen_y2 {
+      y1 = screen_y2;
+    }
+
+    if x2 > screen_x2 {
+      x2 = screen_x2;
+    }
+
+    if y2 > screen_y2 {
+      y2 = screen_y2;
+    }
+
+    if x1 >= x2 || y1 >= y2 {
+      return None;
+    }
+
+    capture_screen_area(&self, x1, y1, (x2 - x1) as u32, (y2 - y1) as u32)
+  }
 }
