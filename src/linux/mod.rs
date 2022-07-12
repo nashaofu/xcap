ae92cd441fb5398858c0c079bb62a07bf3a5c824
nashaofu <diaocheng@outlook.com>
2022-07-12 09:43:09 +0800

    feat: linux X 桌面区域截图

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -6,7 +6,7 @@ use crate::Screen;
 
 use std::env::var_os;
 use wayland::wayland_capture_screen;
-use xorg::xorg_capture_screen;
+use xorg::{xorg_capture_screen, xorg_capture_screen_area};
 
 fn wayland_detect() -> bool {
   let xdg_session_type = var_os("XDG_SESSION_TYPE")
@@ -27,5 +27,19 @@ pub fn capture_screen(screen: &Screen) -> Option<Image> {
     wayland_capture_screen(&screen)
   } else {
     xorg_capture_screen(&screen)
+  }
+}
+
+pub fn capture_screen_area(
+  screen: &Screen,
+  x: i32,
+  y: i32,
+  width: u32,
+  height: u32,
+) -> Option<Image> {
+  if wayland_detect() {
+    wayland_capture_screen(&screen)
+  } else {
+    xorg_capture_screen_area(&screen, x, y, width, height)
   }
 }
--- src/linux/xorg.rs
+++ src/linux/xorg.rs
@@ -2,7 +2,7 @@ use crate::{Image, Screen};
 use std::{ptr, slice};
 use x11::xlib::{XAllPlanes, XCloseDisplay, XDefaultRootWindow, XGetImage, XOpenDisplay, ZPixmap};
 
-pub fn xorg_capture_screen(screen: &Screen) -> Option<Image> {
+fn capture(x: i32, y: i32, width: u32, height: u32) -> Option<Image> {
   unsafe {
     let display_ptr = XOpenDisplay(ptr::null_mut());
 
@@ -11,10 +11,6 @@ pub fn xorg_capture_screen(screen: &Screen) -> Option<Image> {
     }
 
     let window_id = XDefaultRootWindow(display_ptr);
-    let x = ((screen.x as f32) * screen.scale) as i32;
-    let y = ((screen.y as f32) * screen.scale) as i32;
-    let width = ((screen.width as f32) * screen.scale) as u32;
-    let height = ((screen.height as f32) * screen.scale) as u32;
 
     let ximage = XGetImage(
       display_ptr,
@@ -46,4 +42,28 @@ pub fn xorg_capture_screen(screen: &Screen) -> Option<Image> {
       Err(_) => None,
     }
   }
+}
+
+pub fn xorg_capture_screen(screen: &Screen) -> Option<Image> {
+  let x = ((screen.x as f32) * screen.scale) as i32;
+  let y = ((screen.y as f32) * screen.scale) as i32;
+  let width = ((screen.width as f32) * screen.scale) as u32;
+  let height = ((screen.height as f32) * screen.scale) as u32;
+
+  capture(x, y, width, height)
+}
+
+pub fn xorg_capture_screen_area(
+  screen: &Screen,
+  x: i32,
+  y: i32,
+  width: u32,
+  height: u32,
+) -> Option<Image> {
+  let area_x = (((x + screen.x) as f32) * screen.scale) as i32;
+  let area_y = (((y + screen.y) as f32) * screen.scale) as i32;
+  let area_width = ((width as f32) * screen.scale) as u32;
+  let area_height = ((height as f32) * screen.scale) as u32;
+
+  capture(area_x, area_y, area_width, area_height)
 }
