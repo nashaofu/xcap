155bc1af8504d65af3e62962ddce06b3ef1832a5
WilgnerFSDev <wilgner.fabio@gmail.com>
2022-09-20 11:34:01 -0300

    Added support to get all pixels, or a specific pixel from the image and return the position (x,y) and rgb

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -23,5 +23,17 @@ fn main() {
   let buffer = image.buffer();
   fs::write("target/capture_display_with_point.png", &buffer).unwrap();
 
+  // Get the pixels of the image
+  let pixels = image.pixels();
+  for pixel in pixels {
+      if pixel.r == 255 && pixel.g == 255 && pixel.b == 255 {
+          println!("Found white pixel at x: {}, y: {}", pixel.x, pixel.y);
+      }
+  }
+
+  // Get specific pixel
+  let pixel = image.get_pixel(0, 0);
+  println!("Pixel at x: {}, y: {} has rgb values: {}, {}, {}", pixel.x, pixel.y, pixel.r, pixel.g, pixel.b);
+
   println!("运行耗时: {:?}", start.elapsed());
 }
--- src/image.rs
+++ src/image.rs
@@ -4,14 +4,23 @@ pub struct Image {
   width: u32,
   height: u32,
   buffer: Vec<u8>,
+  pixels: Option<Vec<Pixel>>,
+}
+pub struct Pixel {
+  pub x: u32,
+  pub y: u32,
+  pub r: u8,
+  pub g: u8,
+  pub b: u8,
 }
 
 impl Image {
-  pub fn new(width: u32, height: u32, buffer: Vec<u8>) -> Self {
+  pub fn new(width: u32, height: u32, buffer: Vec<u8>, pixels: Vec<Pixel>) -> Self {
     Image {
       width,
       height,
       buffer,
+      pixels: Some(pixels),
     }
   }
 
@@ -19,11 +28,23 @@ impl Image {
     let mut buffer = Vec::new();
     let mut bytes = bgra;
 
-    // BGRA 转换为 RGBA
+    // BGRA convert to RGBA
+    let mut pixels = Vec::new();
     for i in (0..bytes.len()).step_by(4) {
+      // Get RGB values
       let b = bytes[i];
+      let g = bytes[i + 1];
       let r = bytes[i + 2];
 
+      // Get pixel position
+      let x = (i / 4) as u32 % width;
+      let y = (i / 4) as u32 / width;
+
+      // Push pixel to vector
+      let pixel = Pixel { x, y, r, g, b };
+      pixels.push(pixel);
+
+      // Convert to RGBA
       bytes[i] = r;
       bytes[i + 2] = b;
       bytes[i + 3] = 255;
@@ -38,7 +59,8 @@ impl Image {
     writer.write_image_data(&bytes)?;
     writer.finish()?;
 
-    Ok(Image::new(width, height, buffer))
+    // return
+    Ok(Image::new(width, height, buffer, pixels))
   }
 
   pub fn width(&self) -> u32 {
@@ -51,5 +73,14 @@ impl Image {
 
   pub fn buffer(&self) -> &Vec<u8> {
     &self.buffer
+  }
+
+  pub fn pixels(&self) -> &Vec<Pixel> {
+    self.pixels.as_ref().unwrap()
+  }
+
+  // get pixel at x, y and return Pixel struct with x, y, r, g, b values
+  pub fn get_pixel(&self, x: u32, y: u32) -> &Pixel {
+    self.pixels.as_ref().unwrap().iter().find(|pixel| pixel.x == x && pixel.y == y).unwrap()
   }
 }
