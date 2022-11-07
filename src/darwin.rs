01f33e9d7f006dcdedf6e0ac58fd48d627c407ea
nashaofu <diaocheng@outlook.com>
2022-11-07 22:20:09 +0800

    fix: 修复 MacOS capture_area panic

--- src/darwin.rs
+++ src/darwin.rs
@@ -1,5 +1,5 @@
 use crate::{DisplayInfo, Image};
-use core_graphics::display::{CGDisplay, CGPoint, CGRect, CGSize};
+use core_graphics::display::CGDisplay;
 
 pub fn capture_screen(display_info: &DisplayInfo) -> Option<Image> {
   let cg_display = CGDisplay::new(display_info.id);
@@ -21,22 +21,26 @@ pub fn capture_screen_area(
   height: u32,
 ) -> Option<Image> {
   let cg_display = CGDisplay::new(display_info.id);
-  let full_cg_image = cg_display.image()?;
+  let cg_image = cg_display.image()?;
 
-  let w = width as f32 * display_info.scale_factor;
-  let h = height as f32 * display_info.scale_factor;
+  let w = (width as f32 * display_info.scale_factor) as i32;
+  let h = (height as f32 * display_info.scale_factor) as i32;
 
-  let cg_rect = CGRect::new(
-    &CGPoint::new(x as f64, y as f64),
-    &CGSize::new(w as f64, h as f64),
-  );
+  let mut bgra = vec![0; (w * h * 4) as usize];
+  let data = cg_image.data();
+  let bytes = data.bytes();
 
-  let cg_image = full_cg_image.cropped(cg_rect)?;
+  // 图片裁剪
+  for r in y..(y + h) {
+    for c in x..(x + w) {
+      let index = (((r - y) * w + (c - x)) * 4) as usize;
+      let i = ((r * cg_image.width() as i32 + c) * 4) as usize;
+      bgra[index] = bytes[i];
+      bgra[index + 1] = bytes[i + 1];
+      bgra[index + 2] = bytes[i + 2];
+      bgra[index + 3] = bytes[i + 3];
+    }
+  }
 
-  Image::from_bgra(
-    cg_image.width() as u32,
-    cg_image.height() as u32,
-    Vec::from(cg_image.data().bytes()),
-  )
-  .ok()
+  Image::from_bgra(w as u32, h as u32, bgra).ok()
 }
