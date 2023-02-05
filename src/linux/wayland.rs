b35b78eda043f57672d29e6ab907e89e57c3fad5
nashaofu <diaocheng@outlook.com>
2023-02-05 16:02:18 +0800

    feat: 修改 linux 截图返回值为 Result

--- src/linux/wayland.rs
+++ src/linux/wayland.rs
@@ -1,6 +1,7 @@
 use crate::{linux::wayland_screenshot::wayland_screenshot, DisplayInfo, Image};
+use anyhow::{Result};
 
-pub fn wayland_capture_screen(display_info: &DisplayInfo) -> Option<Image> {
+pub fn wayland_capture_screen(display_info: &DisplayInfo) -> Result<Image> {
   let x = ((display_info.x as f32) * display_info.scale_factor) as i32;
   let y = ((display_info.y as f32) * display_info.scale_factor) as i32;
   let width = (display_info.width as f32) * display_info.scale_factor;
@@ -8,7 +9,7 @@ pub fn wayland_capture_screen(display_info: &DisplayInfo) -> Option<Image> {
 
   let buffer = wayland_screenshot(x, y, width as i32, height as i32)?;
 
-  Some(Image::new(width as u32, height as u32, buffer))
+  Ok(Image::new(width as u32, height as u32, buffer))
 }
 
 pub fn wayland_capture_screen_area(
@@ -17,12 +18,12 @@ pub fn wayland_capture_screen_area(
   y: i32,
   width: u32,
   height: u32,
-) -> Option<Image> {
+) -> Result<Image> {
   let area_x = (((x + display_info.x) as f32) * display_info.scale_factor) as i32;
   let area_y = (((y + display_info.y) as f32) * display_info.scale_factor) as i32;
   let area_width = (width as f32) * display_info.scale_factor;
   let area_height = (height as f32) * display_info.scale_factor;
 
   let buffer = wayland_screenshot(area_x, area_y, area_width as i32, area_height as i32)?;
-  Some(Image::new(width as u32, height as u32, buffer))
+  Ok(Image::new(width as u32, height as u32, buffer))
 }
--- src/linux/xorg.rs
+++ src/linux/xorg.rs
@@ -1,11 +1,15 @@
 use crate::{DisplayInfo, Image};
+use anyhow::{anyhow, Result};
 use xcb::x::{Drawable, GetImage, ImageFormat};
 
-fn capture(x: i32, y: i32, width: u32, height: u32) -> Option<Image> {
-  let (conn, index) = xcb::Connection::connect(None).ok()?;
+fn capture(x: i32, y: i32, width: u32, height: u32) -> Result<Image> {
+  let (conn, index) = xcb::Connection::connect(None)?;
 
   let setup = conn.get_setup();
-  let screen = setup.roots().nth(index as usize)?;
+  let screen = setup
+    .roots()
+    .nth(index as usize)
+    .ok_or_else(|| anyhow!("Not found screen"))?;
 
   let get_image_cookie = conn.send_request(&GetImage {
     format: ImageFormat::ZPixmap,
@@ -17,13 +21,14 @@ fn capture(x: i32, y: i32, width: u32, height: u32) -> Option<Image> {
     plane_mask: u32::MAX,
   });
 
-  let get_image_reply = conn.wait_for_reply(get_image_cookie).ok()?;
+  let get_image_reply = conn.wait_for_reply(get_image_cookie)?;
   let bytes = Vec::from(get_image_reply.data());
 
-  Image::from_bgra(width, height, bytes).ok()
+  let image = Image::from_bgra(width, height, bytes)?;
+  Ok(image)
 }
 
-pub fn xorg_capture_screen(display_info: &DisplayInfo) -> Option<Image> {
+pub fn xorg_capture_screen(display_info: &DisplayInfo) -> Result<Image> {
   let x = ((display_info.x as f32) * display_info.scale_factor) as i32;
   let y = ((display_info.y as f32) * display_info.scale_factor) as i32;
   let width = ((display_info.width as f32) * display_info.scale_factor) as u32;
@@ -38,7 +43,7 @@ pub fn xorg_capture_screen_area(
   y: i32,
   width: u32,
   height: u32,
-) -> Option<Image> {
+) -> Result<Image> {
   let area_x = (((x + display_info.x) as f32) * display_info.scale_factor) as i32;
   let area_y = (((y + display_info.y) as f32) * display_info.scale_factor) as i32;
   let area_width = ((width as f32) * display_info.scale_factor) as u32;
