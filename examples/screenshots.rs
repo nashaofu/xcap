2f3c5f5fc5422f94c313706412392794dd942c06
nashaofu <diaocheng@outlook.com>
2023-02-07 23:37:35 +0800

    feat: 优化代码
    * break: windows DisplayInfo.id 生成算法改变，与 electron 的 display_id 不再相同

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -6,22 +6,22 @@ fn main() {
   let screens = Screen::all().unwrap();
 
   for screen in screens {
-    println!("capturer {:?}", screen);
+    println!("capturer {screen:?}");
     let mut image = screen.capture().unwrap();
     let mut buffer = image.buffer();
-    fs::write(format!("target/{}.png", screen.display_info.id), &buffer).unwrap();
+    fs::write(format!("target/{}.png", screen.display_info.id), buffer).unwrap();
 
     image = screen.capture_area(300, 300, 300, 300).unwrap();
     buffer = image.buffer();
-    fs::write(format!("target/{}-2.png", screen.display_info.id), &buffer).unwrap();
+    fs::write(format!("target/{}-2.png", screen.display_info.id), buffer).unwrap();
   }
 
   let screen = Screen::from_point(100, 100).unwrap();
-  println!("capturer {:?}", screen);
+  println!("capturer {screen:?}");
 
   let image = screen.capture_area(300, 300, 300, 300).unwrap();
   let buffer = image.buffer();
-  fs::write("target/capture_display_with_point.png", &buffer).unwrap();
+  fs::write("target/capture_display_with_point.png", buffer).unwrap();
 
   println!("运行耗时: {:?}", start.elapsed());
 }
