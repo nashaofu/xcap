b9b4aab144c279c8156a6741a6f68236fc80dd28
drp4rad0x <87465832+drp4rad0x@users.noreply.github.com>
2023-07-31 13:09:35 +0000

    to_png function updated to improve usability.  (#58)
    * to_png updated. Now user can use value of Compression enum directly without using Some(). Readme and example updated accordingly
    
    * to_png updated. Now user can use value of Compression enum directly without using Some(). Readme and example updated accordingly
    
    ---------
    
    Co-authored-by: user <user@pop-os.localdomain>

--- examples/screenshots.rs
+++ examples/screenshots.rs
@@ -1,5 +1,4 @@
-use png::Compression;
-use screenshots::Screen;
+use screenshots::{Screen, Compression};
 use std::{fs, time::Instant};
 
 fn main() {
@@ -10,7 +9,7 @@ fn main() {
         println!("capturer {screen:?}");
         let mut image = screen.capture().unwrap();
         let mut buffer = image.to_png(None).unwrap();
-        let mut compressed_buffer = image.to_png(Some(Compression::Best)).unwrap();
+        let mut compressed_buffer = image.to_png(Compression::Best).unwrap();
         fs::write(format!("target/{}.png", screen.display_info.id), buffer).unwrap();
         fs::write(
             format!("target/{}-compressed.png", screen.display_info.id),
@@ -19,8 +18,8 @@ fn main() {
         .unwrap();
 
         image = screen.capture_area(300, 300, 300, 300).unwrap();
-        buffer = image.to_png(None).unwrap();
-        compressed_buffer = image.to_png(Some(Compression::Best)).unwrap();
+        buffer = image.to_png(Compression::Fast).unwrap();
+        compressed_buffer = image.to_png(Compression::Best).unwrap();
 
         fs::write(format!("target/{}-2.png", screen.display_info.id), buffer).unwrap();
 
@@ -35,8 +34,8 @@ fn main() {
     println!("capturer {screen:?}");
 
     let image = screen.capture_area(300, 300, 300, 300).unwrap();
-    let buffer = image.to_png(None).unwrap();
-    let compressed_buffer = image.to_png(Some(Compression::Best)).unwrap();
+    let buffer = image.to_png(Compression::Default).unwrap();
+    let compressed_buffer = image.to_png(Compression::Best).unwrap();
 
     fs::write("target/capture_display_with_point.png", buffer).unwrap();
     fs::write("target/capture_display_with_point.png", compressed_buffer).unwrap();
