afebfd6602b8b53319fbec1079fbc16ae98de576
nashaofu <nashaofu@users.noreply.github.com>
2024-12-20 21:51:57 +0800

    chore: fmt

--- examples/window.rs
+++ examples/window.rs
@@ -8,11 +8,11 @@ fn main() {
 
     for window in windows.clone() {
         println!(
-                "Window:\n id: {}\n title: {}\n app_name: {}\n pid: {}\n monitor: {:?}\n position: {:?}\n size {:?}\n state {:?}\n",
-                window.id(),
-                window.title(),
-                window.app_name(),
-                window.pid(),
+            "Window:\n id: {}\n title: {}\n app_name: {}\n pid: {}\n monitor: {:?}\n position: {:?}\n size {:?}\n state {:?}\n",
+            window.id(),
+            window.title(),
+            window.app_name(),
+            window.pid(),
             window.current_monitor().name(),
             (window.x(), window.y(), window.z()),
             (window.width(), window.height()),
