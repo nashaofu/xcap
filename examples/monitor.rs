8291fac43629ca7f2a72e1c11937bcf59f7a5303
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-02-18 23:45:48 +0800

    refactor: Refactor Windows monitors and window attributes to be dynamically calculated.

--- examples/monitor.rs
+++ examples/monitor.rs
@@ -9,22 +9,22 @@ fn main() {
     for monitor in monitors {
         println!(
             "Monitor:\n id: {}\n name: {}\n position: {:?}\n size: {:?}\n state:{:?}\n",
-            monitor.id(),
-            monitor.name(),
-            (monitor.x(), monitor.y()),
-            (monitor.width(), monitor.height()),
+            monitor.id().unwrap(),
+            monitor.name().unwrap(),
+            (monitor.x().unwrap(), monitor.y().unwrap()),
+            (monitor.width().unwrap(), monitor.height().unwrap()),
             (
-                monitor.rotation(),
-                monitor.scale_factor(),
-                monitor.frequency(),
-                monitor.is_primary()
+                monitor.rotation().unwrap(),
+                monitor.scale_factor().unwrap(),
+                monitor.frequency().unwrap(),
+                monitor.is_primary().unwrap()
             )
         );
     }
 
     let monitor = Monitor::from_point(100, 100).unwrap();
 
-    println!("Monitor::from_point(): {}", monitor.name());
+    println!("Monitor::from_point(): {:?}", monitor.name().unwrap());
     println!(
         "Monitor::from_point(100, 100) 运行耗时: {:?}",
         start.elapsed()
