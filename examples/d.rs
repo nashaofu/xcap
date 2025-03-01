abeb25e287f6ac22475c28fae589b7e7a9c6cc82
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-03-01 22:02:16 +0800

    chore: 修改文档与example

--- examples/d.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-use image::RgbaImage;
-
-fn main() {
-    let frame = fs::read("./frame_0.rgb").unwrap();
-    let img = RgbaImage::from_raw(3584, 2240, frame).unwrap();
-    img.save("a.png").unwrap()
-}
